be592ee3d64cda0fde40045df8e09b3f5c1d3020
dhanush614 <dhanushkumar614@gmail.com>
2021-08-17 13:20:10 +0000

    LC

---
+++ src/DateUtil.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class DateUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(getLocalCurrentDate());
+	}
+
+	private static Date getLocalCurrentDate() {
+		return new Date();
+	}
+}
